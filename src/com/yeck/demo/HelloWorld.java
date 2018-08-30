f2526011ee2acd1e00806477eac301ca28c4571a
yeck <yeck_oh@163.com>
2018-08-30 10:45:30 +0800

    IdeaLearning

---
+++ src/com/yeck/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.yeck.demo;
+
+/**
+ * @program: hello-world
+ * @Date: 2018/8/30 10:06
+ * @Author: Mr.Ye
+ * @Description:
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+
+
+}
